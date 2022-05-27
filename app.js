aded49f43087c07d81e578b5110a17b6826822cb
mosesmbugua <swahili1963@gmail.com>
2022-05-26 17:45:43 -0700

    Application Level Middleware

--- app.js
+++ app.js
@@ -3,43 +3,13 @@ const path = require("path");
 const favicon = require("serve-favicon");
 
 let index = require("./routes/index");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
 
-// const config = {};
+const configuration = {};
 
-// var app = express();
-
-// // Static assets.
-// // Move this after the logger if you want to log requests for static assets.
-// // Uncomment when you've added a favicon to your project.
-// app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-// app.use(express.static(path.join(__dirname, "public")));
-
-// // view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "pug");
-
-// app.use("/", index);
-
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   var err = new Error("Not Found");
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in test
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "test" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
-
-// module.exports = app;
-
+if (1) {
+}
 module.exports = () => {
   var app = express();
   // Static assets
@@ -50,6 +20,11 @@ module.exports = () => {
   app.set("views", path.join(__dirname, "views"));
   app.set("view engine", "pug");
 
+  //middleware
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
   //default route
   app.use("/", index);
 
