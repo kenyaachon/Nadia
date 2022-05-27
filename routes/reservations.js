5c4e409d421d274ac95660019703bbc7c861ffb8
mosesmbugua <swahili1963@gmail.com>
2022-05-26 18:38:15 -0700

    Reservation Data Type

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const reservations = require("../lib/reservations");
+const Reservation = require("../lib/schema/reservation");
 
 router.get("/", function (req, res, next) {
   res.render("reservations");
 });
 
 router.post("/", function (req, res, next) {
-  res.render("index");
+  const reservation = new Reservation(req.body);
 });
 module.exports = router;
