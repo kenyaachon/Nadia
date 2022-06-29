2afee6fa237b4a0b070e7f87bea49267af9e506e
mosesmbugua <swahili1963@gmail.com>
2022-06-28 21:47:54 -0700

    Add Tests for Admin Page

--- routes/admin.test.js
+++ routes/admin.test.js
@@ -1,5 +1,27 @@
+const request = require("supertest");
+
+const mockMorgan = jest.fn((req, res, next) => next());
+const mockFetch = jest.fn().mockResolvedValue([]);
+
+let app;
+beforeAll(() => {
+  jest.mock("morgan", () => () => mockMorgan);
+
+  //mock fetching data from the database
+  jest.mock("../lib/knex", () => ({ select: () => ({ table: mockFetch }) }));
+  app = request(require("../app")());
+});
+
+afterAll(() => {
+  jest.unmock("morgan");
+});
+
+//admin page
 describe("GET", () => {
-  it("should return the admin page", () => {
-    expect("admin").toEqual("admin");
+  it("should render admin page", async () => {
+    return await app.get("/admin").auth("admin", "admin").expect(200);
+  });
+  it("should reject admin page access without password", async () => {
+    return await app.get("/admin").expect(401);
   });
 });
